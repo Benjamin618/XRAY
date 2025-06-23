e3e4f5119b3c6ffa7f8dacae2662ed188d88bcbd
Ubuntu <ubuntu@ip-172-31-20-236.eu-west-1.compute.internal>
2025-06-23 14:26:09 +0000

    Ajout Code Section

---
+++ Code/DownloadXRAY.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -L -o ~/XRAY2  https://www.kaggle.com/api/v1/datasets/download/tawsifurrahman/covid19-radiography-database
---
+++ Code/Preprocessing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('/home/ubuntu/XRAY/COVID-19_Radiography_Dataset/Normal.metadata.xlsx') 
+
+print(df.head())
+
+
+
