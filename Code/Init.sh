af7c3770d7b7fd485453ba8cb2ecafd55f5002ae
Ubuntu <ubuntu@ip-172-31-22-31.eu-west-1.compute.internal>
2025-07-02 22:05:33 +0000

    Upgrade Preprocessing Code + add Init bash script

---
+++ Code/Init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# git clone https://github.com/Benjamin618/XRAY
+pip install pandas --no-warn-script-location
+pip install matplotlib --no-warn-script-location
+pip install openpyxl
+pip install opencv-python-headless 
+
