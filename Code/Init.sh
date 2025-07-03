f8e758f8b93910c83d617e119dd8e16a0af32650
Ubuntu <ubuntu@ip-172-31-38-62.eu-west-1.compute.internal>
2025-07-03 16:25:14 +0000

    Upgrade Code

--- Code/Init.sh
+++ Code/Init.sh
@@ -2,6 +2,7 @@
 # git clone https://github.com/Benjamin618/XRAY
 pip install pandas --no-warn-script-location
 pip install matplotlib --no-warn-script-location
+pip install seaborn
 pip install openpyxl
 pip install opencv-python-headless 
 
